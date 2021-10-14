05cc4dd79f51984ab5bdaed020c17c59cb60f0cb
George N <663043+grnassar@users.noreply.github.com>
2021-10-13 19:14:45 -0500

    Update download.php
     - Initial implementation of cache cleanup functionality
     - Initial implementation of delete-when-expired functionality
     - sure, readability is good, I guess, let's do that

--- src/download.php
+++ src/download.php
@@ -10,7 +10,7 @@ $argv0 = $argv[0];
 
 // Parse command line options options
 $offset  = 0;
-$options = getopt("o:g:fswmdxvlth", [], $offset);
+$options = getopt("o:g:fdxswcmvlth", [], $offset);
 
 // Remove command line options from argv, so that only the path/api key remain
 $argv = array_splice($argv, $offset);
@@ -30,6 +30,10 @@ define("TD_VERBOSE", isset($options['v']));
 define("TD_FLATTEN", isset($options['f']));
 define("TD_OVERWRITE", isset($options['w']));
 define("TD_UNATTENDED", TD_OVERWRITE || isset($options['s']));
+define("TD_CLEANUP", isset($options['c']));
+define("TD_VERIFY", isset($options['m']));
+define("TD_EXPUNGE", isset($options['x']));
+define("TD_DELETE", TD_EXPUNGE || isset($options['d']));
 
 define("TD_META_DIR", "_meta"); // set to "." to revert to original upstream functionality
 define("TD_META_PATH", TD_BASE_PATH . DIRECTORY_SEPARATOR . TD_META_DIR);
@@ -48,7 +52,7 @@ if (!is_dir(TD_BASE_PATH)) {
 }
 
 // verify MD5 cache only
-if (isset($options['m'])) verifyMD5();
+if (TD_CLEANUP || TD_VERIFY) checkMD5(TD_VERIFY);
 
 // Trove HTTP Client
 $client = getGuzzleHttpClient($trove_key);
@@ -60,11 +64,16 @@ if (isset($options['t'])) {
     file_put_contents(TD_BASE_PATH . DIRECTORY_SEPARATOR . 'trove-metadata-' . date("Ymd-Hms") . '.json', json_encode($trove_data));
 }
 
-// Do deletes first
-// TODO: implement, obvs
-if (isset($options['x']) || isset($options['d'])) {
-    $allfiles = scandir(TD_BASE_PATH);
-    foreach ($allfiles as $dfile) {
+/* Create current local file list for deletes */
+if (TD_DELETE) {
+    if (TD_VERBOSE) print ("Creating local file list...\n");
+    $local_files = array();
+    foreach (array("windows", "mac", "linux") as $os) {
+        if (in_array($os, $exclude_os)) {
+            continue;
+        }
+        $os_files = getFilespec(TD_BASE_PATH . DIRECTORY_SEPARATOR . $os, 1);
+        $local_files = array_merge($local_files, array_map(fn($a) => $os . DIRECTORY_SEPARATOR . $a, $os_files));
     }
 }
 
@@ -81,6 +90,7 @@ foreach ($trove_data as $game) {
     if (in_array($game_code, $exclude_games)) {
         print "Skipping $display [$game_code] (excluded)...\n";
         $file_results[$game_code] = "skip-game";
+        // TODO: skip game for deletion as well! - maybe break out this file_results per download too?
         continue;
     }
 
@@ -88,11 +98,12 @@ foreach ($trove_data as $game) {
 
     foreach ($downloads as $os => $dl) {
         $file = $dl->url->web;
-        $filename = TD_FLATTEN?basename($file):$file;
+        $filename = TD_FLATTEN ? basename($file) : $file;
         $game = $dl->machine_name;
         $md5  = $dl->md5;
 
-        $dl_path = TD_BASE_PATH . DIRECTORY_SEPARATOR . $os . DIRECTORY_SEPARATOR . $filename;
+        $os_path = $os . DIRECTORY_SEPARATOR . $filename;
+        $dl_path = TD_BASE_PATH . DIRECTORY_SEPARATOR . $os_path;
         // Cache the md5sum in a subdirectory alongside the downloads
         $cache_path = TD_META_PATH . DIRECTORY_SEPARATOR . $os . DIRECTORY_SEPARATOR . dirname($filename) . DIRECTORY_SEPARATOR . "." . basename($filename) . ".md5sum";
 
@@ -101,6 +112,11 @@ foreach ($trove_data as $game) {
             print "   Skipping $os release (excluded)...\n";
             $file_results[$file] = "skip-os";
             continue;
+        }
+
+        // Get this file off our delete list, if it's on it
+        if (TD_DELETE && ($idx = array_search($os_path, $local_files)) !== FALSE) {
+            unset($local_files[$idx]);
         }
 
         // Ensure full path exists on disk
@@ -194,8 +210,8 @@ foreach ($trove_data as $game) {
         } else {
             print "Mismatched md5sum ($new_md5 vs $md5) at $dl_path \n";
             // TODO: do we really need this option? Whether to keep the *file* makes more sense... or maybe not?
-            $filemd5 = readline("Keep file MD5? (Y/N): ");
-            if (strtolower($filemd5) === "y") {
+            $keep_md5 = readline("Keep file MD5? (Y/N): ");
+            if (strtolower($keep_md5) === "y") {
                 file_put_contents($cache_path, $new_md5);
                 $file_results[$file] = "dl-forcedmd5";
             } else {
@@ -213,10 +229,34 @@ foreach ($trove_data as $game) {
 }
 
 print "Processed $count games\n";
+
+if (TD_DELETE) {
+    print ("Deleting games that are no longer in Trove...\n");
+    foreach ($local_files as $del_file) {
+        if (TD_VERBOSE) print ("    Deleting $del_file\n");
+        unlink(TD_BASE_PATH . DIRECTORY_SEPARATOR . $del_file);
+        // TODO: remove empty directories?
+        // TODO: implement delete logging
+        if (TD_EXPUNGE) {
+            $del_md5 = TD_META_PATH . DIRECTORY_SEPARATOR . dirname($del_file) . DIRECTORY_SEPARATOR . "." . basename($del_file) . ".md5sum";
+            if (file_exists($del_md5)) {
+                if (TD_VERBOSE) print ("    Deleting $del_md5\n");
+                unlink($del_md5);
+            }
+        }
+    }
+}
+
 if (isset($options['l'])) {
     // write log file
     file_put_contents(TD_BASE_PATH . DIRECTORY_SEPARATOR . 'trove-dl-' . date("Ymd-Hms") . '.log.json', json_encode($file_results));
 }
+
+
+/************
+ End main
+************/
+
 
 /**
  * Prints usage of script
@@ -229,31 +269,100 @@ function printUsage($program_name, $exitval = 1) {
     print "      -o <os_list>    Comma-separated list of OS to exclude (windows,linux,mac)\n";
     print "      -g <game_list>  Comma-separated list of games to skip (produced in the output of this program in square brackets)\n";
     print "      -f              Flatten directory structure for received games (default: subdirectories are preserved)\n";
+    print "      -d              Delete files no longer in trove (keep hashes) ***\n";
+    print "      -x              Expunge files no longer in trove (delete hashes) ***\n";
+    print "                          *** BE CAREFUL to use the correct -f setting for your repo with these commands!\n";
     print "\n";
     print "      -s              Unattended run, skip games w/mismatched hashes (default: prompt)\n";
     print "      -w              Unattended run, overwrite games w/mismatched hashes\n";
-    print "      -m              Verify MD5 cache vs local files only (no Trove DLs) - not yet implemented\n";
     print "\n";
-    print "      -d              Delete files no longer in trove (keep hashes) - not yet implemented\n";
-    print "      -x              Expunge files no longer in trove (delete hashes) - not yet implemented\n";
+    print "      -c              Check/clean up MD5 cache only, delete old entries (no DL)\n";
+    print "      -m              Like -c, but also verifies MD5 cache vs local files (slow)\n";
     print "\n";
-    print "      -v              Verbose output (beta)\n";
-    print "      -l              Save download log to file\n";
-    print "      -t              Save Trove list to file\n";
+    print "      -v              Verbose output\n";
+    print "      -l              Save a log of the current run to file when done\n";
+    print "      -t              Save any DL'd Trove JSON data to file when done\n";
     print "\n";
     print "      -h              This help text\n\n";
     exit($exitval);
 }
 
 
-function verifyMD5()
-// TODO: implement, obvs
+/**
+ * MD5 hash cache cleanup and verification
+ */
+function checkMD5($verify = false)
+// TODO: finish MD5 correction/generation
+// TODO: implement verification logging
 {
+    $dl_files = $md5_files = array();
     foreach (array("windows","mac","linux") as $os) {
-        $metafiles = scandir();
+        $dl_dir = getFilespec(TD_BASE_PATH . DIRECTORY_SEPARATOR . $os, 1);
+        $md5_dir = getFilespec(TD_META_PATH . DIRECTORY_SEPARATOR . $os, 2);
+        $ostag = fn(&$a) => $a = $os . DIRECTORY_SEPARATOR . $a;
+        array_walk($dl_dir, $ostag);
+        array_walk($md5_dir, $ostag);
+        $dl_files = array_merge($dl_files, $dl_dir);
+        $md5_files = array_merge($md5_files, $md5_dir);
+    }
+    foreach ($md5_files as $md5_file) {
+        $md5_filemap[$md5_file] = file_get_contents(TD_META_PATH . DIRECTORY_SEPARATOR . $md5_file);
+    }
+    print ($verify ? "Verifying hash cache (this will take a while)...\n" : "Checking existing hash cache...\n");
+    foreach ($dl_files as $dl_file) {
+        if (TD_VERBOSE) {
+            print ("  Checking file $dl_file\n");
+        }
+        $md5_path = dirname($dl_file) . DIRECTORY_SEPARATOR . "." . basename($dl_file) . ".md5sum";
+        if (!isset($md5_filemap[$md5_path])) {
+            print ("    $dl_file hash not found in cache\n");
+            // print ("Generating...\n");
+        }
+        else {
+            if ($verify) {
+                $dl_md5 = md5_file(TD_BASE_PATH . DIRECTORY_SEPARATOR . $dl_file);
+            }
+            if ($verify && $md5_filemap[$md5_path] != $dl_md5) {
+                print ("    incorrect hash for $dl_file\n");
+                // print ("Fixing...\n");
+            }
+            unset($md5_filemap[$md5_path]);
+        }
+    }
+    print("Cleaning up unused hash files...\n");
+    foreach ($md5_filemap as $md5_file=>$hashmd5) {
+        print ("  Deleting $md5_file\n");
+        unlink(TD_META_PATH . DIRECTORY_SEPARATOR . $md5_file);
     }
     exit(0);
 }
+
+
+/**
+ * Returns a recursive list of specified files in the passed base+prefix directory, with dir prefix
+ * $filespec = 0 for all files, 1 for all except .md5sum, 2 for only .md5sum
+ * TODO: a user-edited datastore with a circular symlink in it will almost certainly break this
+ */
+function getFilespec($dir_base, $filespec=0, $dir_prefix="") {
+    $this_base = $dir_base . ($dir_prefix == "" ? "" : DIRECTORY_SEPARATOR) . $dir_prefix;
+    $this_files = scandir($this_base);
+    $all_files = array();
+    foreach ($this_files as $currfile) {
+        if ((substr($currfile, -1) == ".") ||
+            ($filespec == 1 && substr($currfile, -7) == ".md5sum") ||
+            ($filespec == 2 && substr($currfile, -7) != ".md5sum")) {
+            continue;
+        }
+        $prefixed_file = $dir_prefix . ($dir_prefix == "" ? "" : DIRECTORY_SEPARATOR) . $currfile;
+        if (is_dir($this_base . DIRECTORY_SEPARATOR . $currfile)) {
+            $all_files = array_merge($all_files, getFilespec($dir_base, $filespec, $prefixed_file));
+        } else {
+            $all_files[] = $prefixed_file;
+        }
+    }
+    return $all_files;
+}
+
 
 /**
  * Creates a Guzzle HTTP Client for interacting with the HB API
